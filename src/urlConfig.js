1d08ce62d7c1e3ef51344925ef8bb1793c1137f8
LQuangTien <tienkg5554@gmail.com>
2022-02-06 13:40:30 +0700

    Update env

--- src/urlConfig.js
+++ src/urlConfig.js
@@ -1,4 +1,6 @@
-export const api = "http://localhost:8080/api";
-// export const api = "https://quangtien-ecommerce-be.herokuapp.com/api";
+export const api =
+  process.env.REACT_APP_MODE === "production"
+    ? "https://quangtien-ecommerce-be.herokuapp.com/api"
+    : "http://localhost:8080/api";
 export const generatePictureUrl = (filename) => filename;
 // `http://localhost:8000/public/${filename}`;
